f05f5ff405a557a1b865629ec0fc13d931aa7651
Anders Launer Bæk-Petersen <anbae@mmmi.sdu.dk>
2025-08-06 05:46:10 +0200

    added script to parse participant list from ITS.

---
+++ infrastructure/parse_its.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+
+def parse_participants(
+    filename: Path,
+    filename_out: Path = "users.txt",
+    prefix: str = "bd-stud",
+    course_desc: str = "bd",
+):
+    """
+    Parse the participants file and write to output file.
+
+    :param filename: Input file containing participant data.
+    :param filename_out: Output file to write parsed data.
+    :param prefix: Prefix to add to each participant's ID.
+    :param course_desc: Course description to append to each line.
+    """
+
+    with open(filename, "r") as file:
+        content = file.readlines()
+
+    content = content[1:]  # Skip the first line
+    content = [line.strip() for line in content if line.strip()]
+    content = [line.split("\t")[1] for line in content]
+    content = [line.removesuffix("@student.sdu.dk") for line in content]
+    if prefix:
+        content = [f"{prefix}-{line}" for line in content]
+
+    content = [f"{line},{course_desc}-{line}" for line in content]
+
+    with open(filename_out, "w") as file:
+        file.write("\n".join(content + [""]))
+
+
+if __name__ == "__main__":
+    parse_participants(Path("its.txt"), Path("tmp-users.txt"))
