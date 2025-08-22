1e1e6e31fa813311b28df3f14fc76886e6f30cdf
Anders Launer Bæk-Petersen <anbae@mmmi.sdu.dk>
2025-08-22 10:27:22 +0200

    sent mails to enigma course.

--- infrastructure/share_kubeconfigs/src/msg.py
+++ infrastructure/share_kubeconfigs/src/msg.py
@@ -9,6 +9,7 @@
 # Port: 465
 # Using: SSL
 
+import enum
 import smtplib
 import ssl
 from dataclasses import dataclass
@@ -17,6 +18,12 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pathlib import Path
+
+
+class SMTPServer(enum.Enum):
+    SDU: dict[str, str | int] = {"host": "smtp.sdu.dk", "port": 25}
+    SDUSSL: dict[str, str | int] = {"host": "smtps.sdu.dk", "port": 465}
+
 
 # batch into N emails
 N_EMAILS = 10
@@ -27,8 +34,13 @@ SLEEP_TIME: int = 60
 class EmailClient:
     email: str
     password: str
-    server: str = "smtps.sdu.dk"
-    port: int = 465
+    smtp_server: SMTPServer = SMTPServer.SDU
+    server: str = None
+    port: int = None
+
+    def __post_init__(self):
+        self.server = self.smtp_server.value["host"]
+        self.port = self.smtp_server.value["port"]
 
     def create_msg(
         self, receiver_email: str, subject: str, body: str, attachment: Path = None
@@ -60,10 +72,21 @@ class EmailClient:
     def send_msg(self, receiver_email: str, msg: str) -> None:
         # Log in to server using secure context and send email
         try:
-            context = ssl.create_default_context()
-            with smtplib.SMTP_SSL(self.server, self.port, context=context) as server:
-                server.login(self.email, self.password)
-                server.sendmail(self.email, receiver_email, msg)
+
+            print(f"Connecting to {self.server}:{self.port} as {self.email}")
+            if self.smtp_server == SMTPServer.SDU:
+                with smtplib.SMTP(self.server, self.port) as server:
+                    print(f"Sending mail to {receiver_email}")
+                    server.sendmail(self.email, receiver_email, msg)
+            elif self.smtp_server == SMTPServer.SDUSSL:
+                context = ssl.create_default_context()
+                with smtplib.SMTP_SSL(
+                    self.server, self.port, context=context
+                ) as server:
+                    server.login(self.email, self.password)
+                    server.sendmail(self.email, receiver_email, msg)
+            else:
+                raise ValueError("Unsupported SMTP server configuration")
         except Exception as e:
             print(f"An error occurred: {e}")
         else:
